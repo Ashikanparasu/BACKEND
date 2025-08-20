b31b52f48a85ab8be2b2be9db042e96951f8a88b
Ashikanparasu <ashikanparasu@gmail.com>
2025-08-21 04:30:36 +0530

    use

--- Express/index.js
+++ Express/index.js
@@ -3,6 +3,11 @@ const app = express()
 
 let port = 8080;
 
+
 app.listen(port, () => {
-    console.log('app listening on port ${port}');
+    console.log(`app listening on port ${port}`);
+});
+
+aap.use((req,res) => {
+    console.log("New incoming request");
 });
